bff45dc110e96da4f199121977280b07999703c8
srujan0403 <srujan1995@yahoo.com>
2023-01-27 11:22:24 -0600

    Sequential model is implemented  for the images

--- ClassifyImages.py
+++ ClassifyImages.py
@@ -1,1 +1,20 @@
 
+
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3,3), activation= 'relu', input_shape=(32,32,3)))
+model.add(layers. MaxPooling2D((2,2)))
+model.add(layers.Conv2D(64, (3,3), activation='relu'))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(64, (3,3), activation='relu'))
+model.add(layers. Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(training_images, training_labels, epochs=100, validation_data=(testing_images, testing_labels))
+
+loss, accuracy = model.evaluate(testing_images, testing_labels)
+print(f"Loss: {loss}")
+print(f"Accuracy: {accuracy}")
