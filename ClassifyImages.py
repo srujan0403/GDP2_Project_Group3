d6cf96fe61a35a003ec0c88a0ad5f08a659ddd94
Anjana Thanguturi <97708371+AnjanaThanguturi@users.noreply.github.com>
2023-01-27 12:08:06 -0600

    27th Jan - Anjana

--- ClassifyImages.py
+++ ClassifyImages.py
@@ -13,8 +13,22 @@ model.add(layers.Dense(10, activation='softmax'))
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
+training_images = training_images[:20000]
+training_labels = training_labels[:20000]
+testing_images = testing_images[:4000]
+testing_labels = testing_labels[:4000]
+
 model.fit(training_images, training_labels, epochs=100, validation_data=(testing_images, testing_labels))
 
 loss, accuracy = model.evaluate(testing_images, testing_labels)
 print(f"Loss: {loss}")
 print(f"Accuracy: {accuracy}")
+
+for i in range(16):
+    plt.subplot(4,4,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.imshow(training_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[training_labels[i][0]])
+
+plt.show()
