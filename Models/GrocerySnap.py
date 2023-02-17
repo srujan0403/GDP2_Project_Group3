ee50ac247455489add964bb9194738d638317f30
Anjana Thanguturi <97708371+AnjanaThanguturi@users.noreply.github.com>
2023-02-17 16:10:39 -0600

    Update in values of old model

--- Models/GrocerySnap.py
+++ Models/GrocerySnap.py
@@ -1,35 +1,31 @@
-import os
 import numpy as np
 import tensorflow as tf
-from tensorflow import keras
-import keras.preprocessing.image
+import keras
+import scipy
 
-
-dataset_path = 'C:\\Users\\S546444\\PycharmProjects\\GrocerySnap\\Grocerydataset\\'
-
+dataset_path = 'C:\\Users\\s546941\\PycharmProjects\\Grocery\\GroceryDataset\\Training'
 
 train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1. / 255,shear_range=0.2,zoom_range=0.2,horizontal_flip=True)
 
 train_generator = train_datagen.flow_from_directory(dataset_path,
-                                                    target_size=(150, 150),
-                                                    batch_size=200,
+                                                    target_size=(100, 100),
+                                                    batch_size=32,
                                                     class_mode='categorical')
 
 
 num_classes = len(train_generator.class_indices)
 classes = train_generator.class_indices
 
-
 model = keras.Sequential([
-    keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(150, 150, 3)),
+    keras.layers.Conv2D(64, (4,4), activation='relu', input_shape=(100, 100, 3)),
     keras.layers.MaxPooling2D(2, 2),
-    keras.layers.Conv2D(64, (3,3), activation='relu'),
+    keras.layers.Conv2D(128, (4,4), activation='relu'),
     keras.layers.MaxPooling2D(2, 2),
-    keras.layers.Conv2D(128, (3,3), activation='relu'),
+    keras.layers.Conv2D(256, (4,4), activation='relu'),
     keras.layers.MaxPooling2D(2, 2),
     keras.layers.Flatten(),
     keras.layers.Dense(512, activation='relu'),
-    keras.layers.Dense(num_classes, activation='softmax')
+    keras.layers.Dense(num_classes, activation='sigmoid')
 ])
 
 
@@ -39,14 +35,15 @@ model.compile(optimizer='adam',
 
 
 history = model.fit_generator(train_generator,
-                              epochs=10,
-                              steps_per_epoch=10)
-
+                              epochs=30,
+                              steps_per_epoch=200)
+train_loss, train_accuracy = model.evaluate_generator(train_generator, steps=len(train_generator))
+print(f'Training loss: {train_loss:.4f}, Training accuracy: {train_accuracy:.4f}')
 
 model.save('fruits_and_vegetables_classifier.h5')
 
 
-test_image = tf.keras.preprocessing.image.load_img('Potato.jpg', target_size=(150, 150))
+test_image = tf.keras.preprocessing.image.load_img('Ava.jpg', target_size=(100, 100))
 test_image = tf.keras.preprocessing.image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis=0)
 
