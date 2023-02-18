3dca27eec4f9e04d43975092a99e367df8b1e69d
Naina Sahiri <98134177+NainaSahiri@users.noreply.github.com>
2023-02-17 23:10:48 -0600

    made changes in the code - Naina Sahiri

--- Models/GrocerySnap-train.py
+++ Models/GrocerySnap-train.py
@@ -67,3 +67,39 @@ history = model.fit_generator(train_generator,
 history = model.fit_generator(train_generator,
                               epochs=40,
                               steps_per_epoch=100)
+
+
+model = keras.Sequential([
+    keras.layers.Conv2D(32 (3,3), activation='relu', input_shape=(150, 150, 3)),
+    keras.layers.MaxPooling2D(2, 2),
+    keras.layers.Conv2D(64, (3,3), activation='relu'),
+    keras.layers.MaxPooling2D(2, 2),
+    keras.layers.Conv2D(128, (3,3), activation='relu'),
+    keras.layers.MaxPooling2D(2, 2),
+    keras.layers.Flatten(),
+    keras.layers.Dense(512, activation='relu'),
+    keras.layers.Dense(num_classes, activation='softmax')
+])
+
+model = keras.Sequential([
+    keras.layers.Conv2D(64, (3,3), activation='relu', input_shape=(150, 150, 3)),
+    keras.layers.MaxPooling2D(2, 2),
+    keras.layers.Conv2D(128, (4,3), activation='relu'),
+    keras.layers.MaxPooling2D(2, 2),
+    keras.layers.Conv2D(64, (3,3), activation='relu'),
+    keras.layers.MaxPooling2D(2, 2),
+    keras.layers.Flatten(),
+    keras.layers.Dense(512, activation='relu'),
+    keras.layers.Dense(num_classes, activation='softmax')
+])
+model = keras.Sequential([
+    keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(160, 160, 4)),
+    keras.layers.MaxPooling2D(2, 2),
+    keras.layers.Conv2D(64, (3,3), activation='relu'),
+    keras.layers.MaxPooling2D(2, 2),
+    keras.layers.Conv2D(128, (2,3), activation='relu'),
+    keras.layers.MaxPooling2D(4, 2),
+    keras.layers.Flatten(),
+    keras.layers.Dense(512, activation='relu'),
+    keras.layers.Dense(num_classes, activation='softmax')
+])
